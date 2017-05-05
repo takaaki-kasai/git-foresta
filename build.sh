dbc9ef14fdaab341f65a2df7004960dc1bfffdb0
Takaaki Kasai <high.river@gmail.com>
2017-05-05 19:13:06 +0900

    fixed build.sh (in dealing with renaming source script)

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ fi
 cp -a local local_tmp
 rm -rf local_tmp/lib/perl5/Module
 rm -rf local_tmp/lib/perl5/Git
-fatpack-simple -d local_tmp git-foresta
+fatpack-simple -d local_tmp -o git-foresta script/git-foresta
 rm -rf local_tmp
